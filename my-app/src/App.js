b9a1bdd5dad7360800de699b1e2ce84448ba0dea
VISALUJ <visaluj2000@gmail.com>
2023-06-22 21:40:29 +0530

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import Weather from './Wheather'
-import "./App.css"
-const App = () => {
+import "./css/main.css";
+import DisplayTodos from "./components/DisplayTodos";
+import Todos from "./components/Todos";
+
+import { motion } from "framer-motion";
+function App() {
   return (
-    <Weather></Weather>
-  )
+    <div className="App">
+      <motion.h1
+        initial={{ y: -200 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 0.5 }}
+        whileHover={{ scale: 1.1 }}
+      >
+        Todo App
+      </motion.h1>
+      <motion.div
+        initial={{ y: 1000 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 1 }}
+      >
+        <Todos />
+        <DisplayTodos />
+      </motion.div>
+    </div>
+  );
 }
 
-export default App 
+export default App;
